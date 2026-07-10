333ffb4d6fad7272be99de6881d63822013b039c
yaminmusleh <yaminaktham3@gmail.com>
2026-07-11 00:55:08 +0300

    Implement initial lexer tokenization system
    - Added TypeOfToken enum to represent different token categories
    - Implemented tokenizer function to scan source code character by character
    - Added support for recognizing return keywords and identifiers
    - Added integer literal detection and token creation
    - Added semicolon token handling
    - Stored generated tokens and added output for debugging lexer results
    - Improved buffer management while processing input characters

--- src/main.cpp
+++ src/main.cpp
@@ -3,37 +3,60 @@
 #include<sstream>
 #include<optional>
 #include<vector>
+#include<cctype>
 using namespace std;
 
 enum class TypeOfToken {
     _return,
     int_lit,
-    semi
+    semi,
+    identifier
 };
 
 struct Token {
     TypeOfToken type;
-    optional<string> value; //the type either contain a value or nothing
+    optional<string> value{};
 };
 
 vector<Token> tokenize(const string &str) {
-    vector<char> buffer{}; //dynamic container
+    //scan characters, build a buffer, create tokens
+    vector<Token> tokens{};
+    string buffer;
+
     for (int i = 0; i < str.length(); i++) {
-        char c = str.at(i); // this should give us the character
+        char c = str.at(i);
+
         if (isalpha(c)) {
-            // checks if a character is an alphabetic character
-            buffer.push_back(c); //used to insert a new element at the very end of a dynamic container
-            //It automatically handles the underlying memory management, increasing the total size of the container by exactly one element.
-            //basically push_back is like working with a stack.
-            i++;
-            while (isalnum(str.at(i))) {
+            buffer.clear();
+
+            while (i < str.length() && isalnum(str.at(i))) {
                 buffer.push_back(str.at(i));
                 i++;
             }
             i--;
+
+            if (buffer == "return") {
+                tokens.push_back(Token{TypeOfToken::_return, buffer});
+            } else {
+                tokens.push_back(Token{TypeOfToken::identifier, buffer});
+            }
+        } else if (isdigit(c)) {
+            buffer.clear();
+
+            while (i < str.length() && isdigit(str.at(i))) {
+                buffer.push_back(str.at(i));
+                i++;
+            }
+            i--;
+            tokens.push_back(Token{TypeOfToken::int_lit, buffer});
+        } else if (c == ';') {
+            tokens.push_back(Token{TypeOfToken::semi, ";"});
         }
     }
+
+    return tokens;
 }
+
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
@@ -42,20 +65,25 @@ int main(int argc, char *argv[]) {
         return EXIT_FAILURE;
     }
 
-    fstream file(argv[1], ios::in); // read the path file because of ios::in,
-    //fstream is capable of reading and writing.
+    fstream file(argv[1], ios::in);
 
     if (!file) {
-        //if there was no file, return an error.
-        cerr << "Failed to open file." << endl;;
+        cerr << "Failed to open file." << endl;
         return EXIT_FAILURE;
     }
 
-
     stringstream buffer;
-    buffer << file.rdbuf(); //read the entire file
+    buffer << file.rdbuf();
+
     string contents = buffer.str();
-    // gives everything that was stored in buffer as string.
-    tokenize(contents);
+
+    vector<Token> tokens = tokenize(contents); // save returned tokens
+
+    // print tokens
+    for (Token token: tokens) {
+        cout << token.value.value() << endl;
+    }
+
+
     return EXIT_SUCCESS;
 }
