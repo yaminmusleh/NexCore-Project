2f1215b91f8a8baa9665015bdce87f049a531237
yaminmusleh <yaminaktham3@gmail.com>
2026-07-11 00:33:56 +0300

    Created a simple lexer mode + not done yet and needs some changes

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include<fstream>
 #include<sstream>
 #include<optional>
+#include<vector>
 using namespace std;
 
 enum class TypeOfToken {
@@ -9,22 +10,44 @@ enum class TypeOfToken {
     int_lit,
     semi
 };
+
 struct Token {
     TypeOfToken type;
-    optional <string> value;//the type either contain a value or nothing
+    optional<string> value; //the type either contain a value or nothing
 };
-int main(int argc, char* argv[]){
-    if (argc !=2) {
-        cerr<<"Incorrect usage. Correct usage is..."<<endl;
-        cerr<<"hyrdo <input.hy>"<<endl;
+
+vector<Token> tokenize(const string &str) {
+    vector<char> buffer{}; //dynamic container
+    for (int i = 0; i < str.length(); i++) {
+        char c = str.at(i); // this should give us the character
+        if (isalpha(c)) {
+            // checks if a character is an alphabetic character
+            buffer.push_back(c); //used to insert a new element at the very end of a dynamic container
+            //It automatically handles the underlying memory management, increasing the total size of the container by exactly one element.
+            //basically push_back is like working with a stack.
+            i++;
+            while (isalnum(str.at(i))) {
+                buffer.push_back(str.at(i));
+                i++;
+            }
+            i--;
+        }
+    }
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        cerr << "Incorrect usage. Correct usage is..." << endl;
+        cerr << "hyrdo <input.hy>" << endl;
         return EXIT_FAILURE;
     }
 
-    fstream file(argv[1],ios::in); // read the path file because of ios::in,
+    fstream file(argv[1], ios::in); // read the path file because of ios::in,
     //fstream is capable of reading and writing.
 
-    if (!file) { //if there was no file, return an error.
-        cerr << "Failed to open file."<<endl;;
+    if (!file) {
+        //if there was no file, return an error.
+        cerr << "Failed to open file." << endl;;
         return EXIT_FAILURE;
     }
 
@@ -33,6 +56,6 @@ int main(int argc, char* argv[]){
     buffer << file.rdbuf(); //read the entire file
     string contents = buffer.str();
     // gives everything that was stored in buffer as string.
-    cout << contents << endl;
+    tokenize(contents);
     return EXIT_SUCCESS;
 }
