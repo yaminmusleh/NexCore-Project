9e501a8753f6dd14e54efc14e7cf94934ce80493
yaminmusleh <yaminaktham3@gmail.com>
2026-07-20 01:14:44 +0300

    created an Arena Allocator that stores a block of memory in a buffer and if it goes out of the scope it will destruct.

--- src/arena.h
+++ src/arena.h
@@ -2,5 +2,22 @@
 
 
 class ArenaAllocator {
-    inline ArenaAllocator();
+public:
+    //i need a constructor and a destructor and a copy constructor
+    inline explicit ArenaAllocator(size_t bytes)
+        : m_size(bytes) //initializer
+    {
+        m_buffer = malloc(m_size);
+    }
+    inline ArenaAllocator(ArenaAllocator& other) = delete;
+
+    inline ArenaAllocator operator = (const ArenaAllocator& other) = delete;
+
+    inline ~ArenaAllocator() {
+        free(m_buffer);
+    }
+
+private:
+    size_t m_size;
+    void* m_buffer; //  a member (pointer) that points to the beginning of the allocated block.
 };
