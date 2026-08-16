a5454af103bba4a956b80425bea379d8849325c2
yaminmusleh <yaminaktham3@gmail.com>
2026-08-17 00:04:28 +0300

    finished implementing for loop and it's ready for use. tested 4 tests to confirm it.

--- src/generation.h
+++ src/generation.h
@@ -9,53 +9,51 @@
 #include <type_traits>
 #include <utility>
 
-
-class Generator {
+class Generator
+{
 public:
     inline explicit Generator(NodeProgram root)
-        : m_root(std::move(root)) {
+        : m_root(std::move(root))
+    {
     }
 
-
-    [[nodiscard]] std::string generate() {
+    [[nodiscard]] std::string generate()
+    {
         m_scopes.clear();
         m_scopes.emplace_back();
         m_labelCount = 0;
         m_variableCount = 0;
 
-
         std::string buffer =
-                "global _start\n"
-                "section .text\n"
-                "_start:\n"
-                "    push rbp\n"
-                "    mov rbp, rsp\n";
-
+            "global _start\n"
+            "section .text\n"
+            "_start:\n"
+            "    push rbp\n"
+            "    mov rbp, rsp\n";
 
         bool has_exit = false;
-
 
         // Generate every statement in the program.
         // Statements can contain nested scopes, so generate_statement()
         // handles recursion.
-        for (auto statement: m_root.statements) {
+        for (auto statement : m_root.statements)
+        {
             if (has_exit)
                 break;
 
             generate_statement(statement, buffer, has_exit);
         }
 
-
         // If the program never called exit(),
         // return 0 by default.
-        if (!has_exit) {
+        if (!has_exit)
+        {
             buffer +=
-                    "    mov rdi, 0\n"
-                    "    mov rax, 60\n"
-                    "    syscall\n";
+                "    mov rdi, 0\n"
+                "    mov rax, 60\n"
+                "    syscall\n";
             return buffer;
         }
-
 
         return buffer;
     }
@@ -69,10 +67,10 @@ private:
     //
     // This temporarily stores a value while another
     // expression is being calculated.
-    void push_temp(std::string &buffer, const std::string &value) {
+    void push_temp(std::string &buffer, const std::string &value)
+    {
         buffer += "    push " + value + "\n";
     }
-
 
     // Removes a temporary value from the stack.
     //
@@ -82,12 +80,13 @@ private:
     //
     // This takes the top temporary value and places it
     // into the requested register.
-    void pop_temp(std::string &buffer, const std::string &reg) {
+    void pop_temp(std::string &buffer, const std::string &reg)
+    {
         buffer += "    pop " + reg + "\n";
     }
 
-
-    struct Var {
+    struct Var
+    {
         // The position of the variable relative to rbp.
         //
         // Example:
@@ -103,7 +102,8 @@ private:
 
     void generate_if(NodeStmntIf *stmt,
                      std::string &buffer,
-                     bool &has_exit) {
+                     bool &has_exit)
+    {
         std::string falseLabel = ".L" + std::to_string(m_labelCount++);
         std::string endLabel = ".L" + std::to_string(m_labelCount++);
 
@@ -119,9 +119,12 @@ private:
 
         bool else_exit = false;
 
-        if (stmt->else_if) {
+        if (stmt->else_if)
+        {
             generate_if(stmt->else_if, buffer, else_exit);
-        } else if (stmt->else_scope) {
+        }
+        else if (stmt->else_scope)
+        {
             generate_scope(stmt->else_scope, buffer, else_exit);
         }
 
@@ -130,14 +133,52 @@ private:
         has_exit = if_exit && else_exit;
     }
 
+    void generate_for(NodeStmntFor *stmt, std::string &buffer, bool &has_exit)
+    {
+        std::string startLabel = ".L" + std::to_string(m_labelCount++);
+        std::string endLabel = ".L" + std::to_string(m_labelCount++);
+
+        generate_statement(stmt->init, buffer, has_exit); // generate initialization once
+
+        buffer += startLabel + ":\n"; // start of the loop
+
+        generate_condition(stmt->condition, buffer, endLabel); // check condition, if false jump to endLabel
+
+        bool body_exit = false;
+
+        generate_scope(stmt->scope, buffer, body_exit); // generate the body of the loop { ... }
+
+        if (!body_exit)
+        {
+            generate_statement(stmt->increment, buffer, has_exit); // generate increment statement after the body of the loop
+            // If the body did not exit the program, go back and check the condition again.
+            // this is like c++ when it checks the condition of the loop after executing the body of the loop.
+            buffer += "    jmp " + startLabel + "\n";
+        }
+        buffer += endLabel + ":\n";
+        has_exit = body_exit;
+        /*
+        The for loop is generated as follows:
+        1. Generate the initialization statement once before the loop starts. (generate_statement)
+        2. Generate the condition check at the start of each iteration. (generate_condition)
+        3. Generate the body of the loop. (generate_scope)
+        4. Generate the increment statement after the body of the loop. (generate_statement) its a statement because we declared it as a statement in the AST.
+        5. If the body did not exit the program, go back and check the condition again. (buffer += "    jmp " + startLabel + "\n";) go back to start_label to check the condition again.
+        6. If the condition is false, jump to the end label and exit the loop. (generate_condition will jump to endLabel if the condition is false)
+        7. The end label marks the end of the loop.
+        This is similar to how a for loop works in C++.
+        */
+    }
+
     void generate_whilst(NodeStmntWhile *stmt,
                          std::string &buffer,
-                         bool &has_exit) {
+                         bool &has_exit)
+    {
         std::string startLabel =
-                ".L" + std::to_string(m_labelCount++);
+            ".L" + std::to_string(m_labelCount++);
 
         std::string endLabel =
-                ".L" + std::to_string(m_labelCount++);
+            ".L" + std::to_string(m_labelCount++);
 
         // Start of the loop.
         buffer += startLabel + ":\n";
@@ -149,8 +190,7 @@ private:
         generate_condition(
             stmt->condition,
             buffer,
-            endLabel
-        );
+            endLabel);
 
         // Generate the body.
         bool body_exit = false;
@@ -158,12 +198,12 @@ private:
         generate_scope(
             stmt->scope,
             buffer,
-            body_exit
-        );
+            body_exit);
 
         // If the body did not exit the program,
         // go back and check the condition again.
-        if (!body_exit) {
+        if (!body_exit)
+        {
             buffer += "    jmp " + startLabel + "\n";
         }
 
@@ -172,7 +212,6 @@ private:
 
         has_exit = body_exit;
     }
-
 
     // Generates a single statement.
     //
@@ -186,8 +225,10 @@ private:
     // so this function calls itself recursively.
     void generate_statement(NodeStmnt *statement,
                             std::string &buffer,
-                            bool &has_exit) {
-        std::visit([&](auto &&stmt) {
+                            bool &has_exit)
+    {
+        std::visit([&](auto &&stmt)
+                   {
             using T = std::decay_t<decltype(stmt)>; // stmt accepts all types of data.
 
 
@@ -243,7 +284,11 @@ private:
                 generate_if(stmt, buffer, has_exit);
             } else if constexpr (std::is_same_v<T, NodeStmntWhile *>) {
                 generate_whilst(stmt, buffer, has_exit);
-            } else if constexpr (std::is_same_v<T, NodeStmntAssign>) {
+            }
+            else if constexpr (std::is_same_v<T, NodeStmntFor *>) {
+                generate_for(stmt, buffer, has_exit);
+            }
+             else if constexpr (std::is_same_v<T, NodeStmntAssign>) {
                 std::string name = stmt.identifier;
 
                 generate_expr(stmt.expr, buffer);
@@ -254,33 +299,32 @@ private:
                 buffer += "], rbx\n";
             } else if constexpr (std::is_same_v<T, NodeScope *>) {
                 generate_scope(stmt, buffer, has_exit);
-            }
-        }, statement->stmnt);
+            } }, statement->stmnt);
     }
-
 
     void generate_scope(NodeScope *scope,
                         std::string &buffer,
-                        bool &has_exit) {
+                        bool &has_exit)
+    {
         size_t oldVariableCount = m_variableCount;
 
         m_scopes.emplace_back();
 
-
-        for (auto child: scope->statements) {
+        for (auto child : scope->statements)
+        {
             generate_statement(child, buffer, has_exit);
 
             if (has_exit)
                 break;
         }
 
-
-        if (!has_exit) {
+        if (!has_exit)
+        {
             size_t variablesCreated =
-                    m_variableCount - oldVariableCount;
+                m_variableCount - oldVariableCount;
 
-
-            if (variablesCreated > 0) {
+            if (variablesCreated > 0)
+            {
                 buffer += "    add rsp, ";
                 buffer += std::to_string(variablesCreated * 8);
                 buffer += "\n";
@@ -289,30 +333,33 @@ private:
             }
         }
 
-
         m_scopes.pop_back();
     }
 
     void generate_condition_branch(NodeExpr *condition,
                                    std::string &buffer,
                                    const std::string &trueLabel,
-                                   const std::string &falseLabel) {
-        if (std::holds_alternative<UnaryExpr *>(condition->expr)) {
+                                   const std::string &falseLabel)
+    {
+        if (std::holds_alternative<UnaryExpr *>(condition->expr))
+        {
             auto unary = std::get<UnaryExpr *>(condition->expr);
 
-            if (unary->op == "!") {
+            if (unary->op == "!")
+            {
                 generate_condition_branch(
                     unary->expr,
                     buffer,
                     falseLabel,
                     trueLabel
-                    //as i see i swapped true label and false label because this is how Logical NOT works.
+                    // as i see i swapped true label and false label because this is how Logical NOT works.
                 );
 
                 return;
             }
         }
-        if (!std::holds_alternative<BinaryExpr *>(condition->expr)) {
+        if (!std::holds_alternative<BinaryExpr *>(condition->expr))
+        {
             generate_expr(condition, buffer);
 
             buffer += "    cmp rbx, 0\n";
@@ -321,14 +368,13 @@ private:
             return;
         }
 
-
         auto binary = std::get<BinaryExpr *>(condition->expr);
 
-
         // true && true
-        if (binary->op == "&&") {
+        if (binary->op == "&&")
+        {
             std::string next =
-                    ".L" + std::to_string(m_labelCount++);
+                ".L" + std::to_string(m_labelCount++);
 
             generate_condition_branch(binary->left,
                                       buffer,
@@ -345,11 +391,11 @@ private:
             return;
         }
 
-
         // true || true
-        if (binary->op == "||") {
+        if (binary->op == "||")
+        {
             std::string next =
-                    ".L" + std::to_string(m_labelCount++);
+                ".L" + std::to_string(m_labelCount++);
 
             generate_condition_branch(binary->left,
                                       buffer,
@@ -366,7 +412,6 @@ private:
             return;
         }
 
-
         // comparison
         generate_expr(binary->left, buffer);
 
@@ -378,37 +423,49 @@ private:
 
         buffer += "    cmp rax, rbx\n";
 
-
-        if (binary->op == "==") {
+        if (binary->op == "==")
+        {
             buffer += "    je " + trueLabel + "\n";
             buffer += "    jmp " + falseLabel + "\n";
-        } else if (binary->op == "!=") {
+        }
+        else if (binary->op == "!=")
+        {
             buffer += "    jne " + trueLabel + "\n";
             buffer += "    jmp " + falseLabel + "\n";
-        } else if (binary->op == "<") {
+        }
+        else if (binary->op == "<")
+        {
             buffer += "    jl " + trueLabel + "\n";
             buffer += "    jmp " + falseLabel + "\n";
-        } else if (binary->op == "<=") {
+        }
+        else if (binary->op == "<=")
+        {
             buffer += "    jle " + trueLabel + "\n";
             buffer += "    jmp " + falseLabel + "\n";
-        } else if (binary->op == ">") {
+        }
+        else if (binary->op == ">")
+        {
             buffer += "    jg " + trueLabel + "\n";
             buffer += "    jmp " + falseLabel + "\n";
-        } else if (binary->op == ">=") {
+        }
+        else if (binary->op == ">=")
+        {
             buffer += "    jge " + trueLabel + "\n";
             buffer += "    jmp " + falseLabel + "\n";
-        } else {
+        }
+        else
+        {
             throw std::runtime_error(
-                "Unknown condition operator: " + binary->op
-            );
+                "Unknown condition operator: " + binary->op);
         }
     }
 
     void generate_condition(NodeExpr *condition,
                             std::string &buffer,
-                            const std::string &falseLabel) {
+                            const std::string &falseLabel)
+    {
         std::string trueLabel =
-                ".L" + std::to_string(m_labelCount++);
+            ".L" + std::to_string(m_labelCount++);
 
         generate_condition_branch(condition,
                                   buffer,
@@ -419,8 +476,10 @@ private:
     }
 
     void generate_expr(NodeExpr *expr,
-                       std::string &buffer) {
-        std::visit([&](auto &&node) {
+                       std::string &buffer)
+    {
+        std::visit([&](auto &&node)
+                   {
             using T = std::decay_t<decltype(node)>;
 
             // Integer literal
@@ -501,10 +560,8 @@ private:
                         "Unknown unary operator: " + node->op
                     );
                 }
-            }
-        }, expr->expr);
+            } }, expr->expr);
     }
-
 
     // Finds a variable by searching from the newest scope
     // to the oldest scope.
@@ -516,30 +573,26 @@ private:
     // }
     //
     // to temporarily hide an outer variable named a.
-    Var lookup(const std::string &name) {
+    Var lookup(const std::string &name)
+    {
         for (auto it = m_scopes.rbegin();
              it != m_scopes.rend();
-             ++it) {
+             ++it)
+        {
             auto found = it->find(name);
-
 
             if (found != it->end())
                 return found->second;
         }
 
-
         throw std::runtime_error(
-            "Unknown variable: " + name
-        );
+            "Unknown variable: " + name);
     }
-
 
     NodeProgram m_root;
 
-
     // Stores variables for each scope.
-    std::vector<std::unordered_map<std::string, Var> > m_scopes;
-
+    std::vector<std::unordered_map<std::string, Var>> m_scopes;
 
     // Counts how many variables exist.
     size_t m_variableCount = 0;
